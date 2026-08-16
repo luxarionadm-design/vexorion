747b1f4219c82b1dd7ea7e54e8b9ed1d2edfd675
luxarionadm-design <luxarion.adm@gmail.com>
2026-08-17 00:26:43 +0700

    Add standard UUID namespaces and default namespace

---
+++ src/constants/Namespaces.js
@@ -0,0 +1,16 @@
+/**
+ * Standard UUID namespaces
+ * @module Namespaces
+ */
+
+export const NAMESPACES = {
+  DNS: '6ba7b810-9dad-11d1-80b4-00c04fd430c8',
+  URL: '6ba7b811-9dad-11d1-80b4-00c04fd430c8',
+  OID: '6ba7b812-9dad-11d1-80b4-00c04fd430c8',
+  X500: '6ba7b814-9dad-11d1-80b4-00c04fd430c8'
+};
+
+export const DEFAULT_NAMESPACE = NAMESPACES.DNS;
+export const VERSION = '5.0.0';
+
+export default NAMESPACES;
